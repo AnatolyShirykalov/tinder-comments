5b4a04f1c07535f2820e8a3bfb3a82f5aa577a0c
AnatolyShirykalov <pipocavsobake@gmail.com>
2018-05-09 16:47:01 +0300

    tinder works

--- app/src/main/java/ru/shirykalov/anatoly/classiconline/MainActivity.java
+++ app/src/main/java/ru/shirykalov/anatoly/classiconline/MainActivity.java
@@ -1,10 +1,10 @@
 package ru.shirykalov.anatoly.classiconline;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +12,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.mindorks.placeholderview.SwipeDecor;
+import com.mindorks.placeholderview.SwipePlaceHolderView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private SwipePlaceHolderView mSwipeView;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,7 +90,7 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
+            tinderCreate();
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
@@ -98,4 +105,38 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void tinderCreate() {
+
+        mSwipeView = (SwipePlaceHolderView) findViewById(R.id.swipeView);
+        mContext = getApplicationContext();
+
+        mSwipeView.getBuilder()
+                .setDisplayViewCount(3)
+                .setSwipeDecor(new SwipeDecor()
+                        .setPaddingTop(20)
+                        .setRelativeScale(0.01f)
+                        .setSwipeInMsgLayoutId(R.layout.tinder_swipe_in_msg_view)
+                        .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
+
+
+        for (Profile profile : Utils.loadProfiles(this.getApplicationContext())) {
+            mSwipeView.addView(new TinderCard(mContext, profile, mSwipeView));
+        }
+
+//        findViewById(R.id.rejectBtn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mSwipeView.doSwipe(false);
+//            }
+//        });
+//
+//        findViewById(R.id.acceptBtn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mSwipeView.doSwipe(true);
+//            }
+//        });
+    }
+
 }
